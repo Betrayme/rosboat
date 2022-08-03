c5d0bf83f51caa3cc4b29ddac415c1170962c340
Alvari <496199270@qq.com>
2022-08-03 10:47:19 +0800

    1.0beta220803

---
+++ src/1.0.0.080322_beta/rosboat/clnt/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alvari/Myfile/workspace/rosboat/clnt
+# Build directory: /home/alvari/Myfile/workspace/rosboat/clnt/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/1.0.0.080322_beta/rosboat/serv/server.cpp
@@ -0,0 +1,38 @@
+#include "UDP4.h"
+
+
+
+int main(int argc,char** argv)
+{
+   if(argc==4)
+   {
+      if(servinteract(argv[1],argv[2],argv[3])==-1)
+      {
+         cout<<"请输入正确的参数~"<<endl;
+         cout<<"-参数 ip port"<<"或 -参数 port"<<endl;
+         cout<<"参数列表:"<<endl;
+         cout<<"-tcp     以tcp套接字创建服务端"<<endl;
+         cout<<"-udp     以udp套接字创建服务端"<<endl;
+      }
+   }
+   if(argc==3)
+   {
+      if(servinteract(argv[1],argv[2],argv[3])==-1)
+      {
+         cout<<"请输入正确的参数~"<<endl;
+         cout<<"-参数 ip port"<<"或 -参数 port"<<endl;
+         cout<<"参数列表:"<<endl;
+         cout<<"-tcp     以tcp套接字创建服务端"<<endl;
+         cout<<"-udp     以udp套接字创建服务端"<<endl;
+      }
+   }
+   else
+   {
+   cout<<"请输入正确的参数~"<<endl;
+   cout<<"-参数 ip port"<<"或 -参数 port"<<endl;
+   cout<<"参数列表:"<<endl;
+   cout<<"-tcp     以tcp套接字创建服务端"<<endl;
+   cout<<"-udp     以udp套接字创建服务端"<<endl;
+   }
+   return 0;
+}
