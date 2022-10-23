1de9672fd8bf69fa9c7b7fefd1d29ee885ab4b98
Ou yakukai <101096283+DwbArookie@users.noreply.github.com>
2022-10-23 11:03:25 +0800

    Add files via upload

--- src/3.1_beta/servwindows/serv.cpp
+++ src/3.1_beta/servwindows/serv.cpp
@@ -27,11 +27,11 @@ int main(int argc,char**argv)
    }
    else
    {
-   cout<<"请输入正确的参数~"<<endl;
-   cout<<"-参数 ip port"<<"或 -参数 port"<<endl;
-   cout<<"参数列表:"<<endl;
-   cout<<"-tcp     以tcp套接字创建服务端"<<endl;
-   cout<<"-udp     以udp套接字创建服务端"<<endl;
+      char jud;
+      cout << "无外部参数，将以默认参数启动(udp@192.168.137.1:8888)(y/n)?" << endl;
+      cin >> jud;
+      if(jud=='y'||jud=='Y')
+         interudp4("-udp", "192.168.137.1", "8888");
    }
     return 0;
 }
